3bfed668f02e059a1be1b3df1c3f8fdad1cdc779
Ernesto Tham <ernesto.tham@kbslp.com>
2021-12-07 14:43:42 -0500

    adjusted UI layout for multi screens

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -105,7 +105,7 @@ function displayResults() {
         console.log("wind "+ weathercast.daily[i].wind_speed)
         console.log("humidity "+ weathercast.daily[i].humidity)
         $('#Fday'.concat(i)).children().remove()
-        $('#Fday'.concat(i)).append('<h1 class="disaply-4 text-center" id="Fday1date">'+convertUniUTCtoLocal(weathercast.daily[i].dt)+'</h1><img src="http://openweathermap.org/img/w/'+weathercast.daily[i].weather[0].icon+'.png" alt="img.jpg"><h1 class="disaply-4 text-center" id="Fday1">Temp: '+weathercast.daily[i].temp.day+' F</h1><h1 class="disaply-4 text-center" id="Fday1">Wind: '+weathercast.daily[i].wind_speed+' MPH</h1><h1 class="disaply-4 text-center" id="Fday1">Humidity: '+weathercast.daily[i].humidity+' %</h1>')
+        $('#Fday'.concat(i)).append('<h1 class="text-start" id="Fday1date">'+convertUniUTCtoLocal(weathercast.daily[i].dt)+'</h1><img src="http://openweathermap.org/img/w/'+weathercast.daily[i].weather[0].icon+'.png" alt="img.jpg"><h3 class="text-start" id="Fday1">Temp: '+weathercast.daily[i].temp.day+' F</h3><h3 class="text-start" id="Fday1">Wind: '+weathercast.daily[i].wind_speed+' MPH</h3><h3 class="text-start" id="Fday1">Humidity: '+weathercast.daily[i].humidity+' %</h3>')
         }
 
         if(i===0){
@@ -115,7 +115,7 @@ function displayResults() {
         console.log("humidity "+ weathercast.daily[i].humidity)
         console.log("UV Index "+ weathercast.daily[i].uvi)
         Fordcastday0.children().remove()
-        Fordcastday0.append('<h1 class="disaply-4 text-center" id="Fday">'+cord.name+" "+convertUniUTCtoLocal(weathercast.daily[i].dt)+'<img src="http://openweathermap.org/img/w/'+weathercast.daily[i].weather[0].icon+'.png" alt="img.jpg"></h1><h1 class="disaply-4 text-center" id="Fday">Temp: '+weathercast.daily[i].temp.day+' F</h1><h1 class="disaply-4 text-center" id="Fday">Wind: '+weathercast.daily[i].wind_speed+' MPH</h1><h1 class="disaply-4 text-center" id="Fday">Humidity: '+weathercast.daily[i].humidity+' %</h1><h1 class="disaply-4 text-center" id="Fday">UV Index: '+weathercast.daily[i].uvi+' </h1>')
+        Fordcastday0.append('<h1 class="text-start" id="Fday">'+cord.name+" "+convertUniUTCtoLocal(weathercast.daily[i].dt)+'<img src="http://openweathermap.org/img/w/'+weathercast.daily[i].weather[0].icon+'.png" alt="img.jpg"></h1><h3 class="text-start" id="Fday">Temp: '+weathercast.daily[i].temp.day+' F</h3><h3 class="text-start" id="Fday">Wind: '+weathercast.daily[i].wind_speed+' MPH</h3><h3 class="text-start" id="Fday">Humidity: '+weathercast.daily[i].humidity+' %</h3><h3 class="text-start" id="Fday">UV Index: '+weathercast.daily[i].uvi+' </h3>')
 
 
         }
@@ -128,7 +128,7 @@ function displayResults() {
 
     }
 
-    var cachedBtn = $('#cachedBtn').append('<button class="btn btn-light p-1 bg-secondary max-height:10px" id="newbtn"  from-control-lg>'+cord.name+'</button>')
+    var cachedBtn = $('#cachedBtn').append('<button class="btn btn-light p-1 w-100 gap-3 bg-secondary max-height:10px" id="newbtn"  from-control-lg>'+cord.name+'</button>')
 
     for(i=0; i < hiddenEls.length; i++){
         hiddenEls[i].classList.remove('d-none')
