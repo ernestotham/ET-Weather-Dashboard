56fdfbc0d2cff6f235d0bfc5ac957ca7f61051b6
Ernesto Tham <ernesto.tham@kbslp.com>
2021-12-08 22:17:00 -0500

    adjusted backgroup color for UV index

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -116,7 +116,7 @@ function load(){
         console.log("wind "+ weathercast.daily[i].wind_speed)
         console.log("humidity "+ weathercast.daily[i].humidity)
         $('#Fday'.concat(i)).children().remove()
-        $('#Fday'.concat(i)).append('<h1 class="text-start" id="Fday1date">'+convertUniUTCtoLocal(weathercast.daily[i].dt)+'</h1><img src="http://openweathermap.org/img/w/'+weathercast.daily[i].weather[0].icon+'.png" alt="img.jpg"><h3 class="text-start" id="Fday1">Temp: '+weathercast.daily[i].temp.day+' F</h3><h3 class="text-start" id="Fday1">Wind: '+weathercast.daily[i].wind_speed+' MPH</h3><h3 class="text-start" id="Fday1">Humidity: '+weathercast.daily[i].humidity+' %</h3>')
+        $('#Fday'.concat(i)).append('<h1 class="text-start" id="Fday1date">'+convertUniUTCtoLocal(weathercast.daily[i].dt)+'</h1><img src="http://openweathermap.org/img/w/'+weathercast.daily[i].weather[0].icon+'.png" alt="img.jpg"><h3 class="text-start" id="Fday1">Temp: '+weathercast.daily[i].temp.day+' F</h3><h3 class="text-start" id="Fday1">Wind: '+weathercast.daily[i].wind_speed+' MPH</h3><h3 class="text-start" id="Fday1">Humidity: '+weathercast.daily[i].humidity+'%</h3>')
         }
 
         if(i===0){
@@ -126,7 +126,7 @@ function load(){
         console.log("humidity "+ weathercast.daily[i].humidity)
         console.log("UV Index "+ weathercast.daily[i].uvi)
         Fordcastday0.children().remove()
-        Fordcastday0.append('<h1 class="text-start" id="Fday">'+cord.name+" "+convertUniUTCtoLocal(weathercast.daily[i].dt)+'<img src="http://openweathermap.org/img/w/'+weathercast.daily[i].weather[0].icon+'.png" alt="img.jpg"></h1><h3 class="text-start" id="Fday">Temp: '+weathercast.daily[i].temp.day+' F</h3><h3 class="text-start" id="Fday">Wind: '+weathercast.daily[i].wind_speed+' MPH</h3><h3 class="text-start" id="Fday">Humidity: '+weathercast.daily[i].humidity+' %</h3><h3 class="text-start" id="Fday">UV Index: '+weathercast.daily[i].uvi+' </h3>')
+        Fordcastday0.append('<h1 class="text-start" id="Fday">'+cord.name+" "+convertUniUTCtoLocal(weathercast.daily[i].dt)+'<img src="http://openweathermap.org/img/w/'+weathercast.daily[i].weather[0].icon+'.png" alt="img.jpg"></h1><h3 class="text-start" id="Fday">Temp: '+weathercast.daily[i].temp.day+' F</h3><h3 class="text-start" id="Fday">Wind: '+weathercast.daily[i].wind_speed+' MPH</h3><h3 class="text-start" id="Fday">Humidity: '+weathercast.daily[i].humidity+' %</h3><h3 class="text-start" id="Fday">UV Index: <span style="display:inline; border-radius: 15px; width: 30px; background-color: green; margin: 2px; padding: 2px 20px 2px 20px; color: white;">'+weathercast.daily[i].uvi+'</span> </h3>')
 
 
         }
